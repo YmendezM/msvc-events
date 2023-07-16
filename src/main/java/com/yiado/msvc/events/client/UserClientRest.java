7150a65edee006a24995c670d9a8d051442a434b
YmendezM <ysmendezm@gmail.com>
2023-07-15 21:11:06 -0400

    Communication environment variable

--- src/main/java/com/yiado/msvc/events/client/UserClientRest.java
+++ src/main/java/com/yiado/msvc/events/client/UserClientRest.java
@@ -11,9 +11,10 @@ import java.util.Optional;
 //call local
 //@FeignClient(name="msvc-users", url="localhost:8001")
 //call Docker container - call Docker container
-@FeignClient(name="msvc-users", url="msvc-users:8001")
+//@FeignClient(name="msvc-users", url="msvc-users:8001")
+//call Docker container - Docker container - Variable config
+@FeignClient(name="msvc-users", url="${msvc.users.url}")
 public interface UserClientRest {
-
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id);
 
